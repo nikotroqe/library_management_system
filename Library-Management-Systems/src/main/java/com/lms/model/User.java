498d34e9eedacaad691021bf392e02805799435f
niko.troqe <troqeniko@gmail.com>
2024-11-17 19:18:36 +0100

    library management system

---
+++ Library-Management-Systems/src/main/java/com/lms/model/User.java
@@ -0,0 +1,36 @@
+package com.lms.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.ZonedDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "createdDate")
+    private ZonedDateTime createdDate;
+}
---
+++ Library-Management-Systems/src/main/java/com/lms/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.lms.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository {
+}
